e2d40cd9e673f96dd8dc896410e5f54bf59ebf18
probir ghosh <probirghosh@outlook.com>
2023-03-09 13:16:23 +0600

    empty search error message issue fixed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const searchFood = () => {
     }
         
     // ----------load data----------
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayMealInfo(data))
